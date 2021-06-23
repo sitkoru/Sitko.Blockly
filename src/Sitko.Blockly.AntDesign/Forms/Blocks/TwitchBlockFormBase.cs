04863abd7e8c129322fb7e7806b908d92c310751
George Drak <sonicgd@gmail.com>
2021-06-23 13:05:17 +0500

    Move url block render logic to base classes

--- src/Sitko.Blockly.AntDesign/Forms/Blocks/TwitchBlockFormBase.cs
+++ src/Sitko.Blockly.AntDesign/Forms/Blocks/TwitchBlockFormBase.cs
@@ -1,13 +1,53 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
 using Sitko.Blockly.Blocks;
 
 namespace Sitko.Blockly.AntDesignComponents.Forms.Blocks
 {
     public abstract class TwitchBlockFormBase : BaseBlockForm<TwitchBlock>
     {
+        [Inject] private IJSRuntime JsRuntime { get; set; } = null!;
+
+        protected virtual bool RenderOnInit => true;
+
         protected override FieldIdentifier CreateFieldIdentifier()
         {
             return FieldIdentifier.Create(() => Block.ChannelId);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender && RenderOnInit)
+            {
+                await JsRuntime.InvokeVoidAsync("Blockly.Twitch.load");
+                await RenderVideoAsync();
+            }
+        }
+
+        protected async Task RenderVideoAsync()
+        {
+            if (!string.IsNullOrEmpty(Block.Url))
+            {
+                var arg = new
+                {
+                    video = Block.VideoId,
+                    channel = Block.ChannelId,
+                    collection = Block.CollectionId,
+                    id = Block.Id,
+                    instance = DotNetObjectReference.Create(this)
+                };
+                await JsRuntime.InvokeAsync<string>(
+                    "Blockly.Twitch.render",
+                    arg);
+            }
+            else
+            {
+                var arg = new {id = Block.Id};
+                await JsRuntime.InvokeAsync<string>("Blockly.Twitch.clear", arg);
+            }
         }
     }
 }
--- src/Sitko.Blockly.AntDesign/Forms/Blocks/TwitterBlockFormBase.cs
+++ src/Sitko.Blockly.AntDesign/Forms/Blocks/TwitterBlockFormBase.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.AspNetCore.Components.Forms;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.JSInterop;
 using Sitko.Blockly.Blocks;
 
 namespace Sitko.Blockly.AntDesignComponents.Forms.Blocks
 {
     public abstract class TwitterBlockFormBase : BaseBlockForm<TwitterBlock>
     {
+        [Inject] private IJSRuntime JsRuntime { get; set; } = null!;
+        protected virtual bool RenderOnInit => true;
+
         protected override FieldIdentifier CreateFieldIdentifier()
         {
             return FieldIdentifier.Create(() => Block.TweetId);
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await base.OnAfterRenderAsync(firstRender);
+            if (firstRender && RenderOnInit)
+            {
+                await RenderTweetAsync();
+            }
+        }
+
+        protected async Task RenderTweetAsync()
+        {
+            if (!string.IsNullOrEmpty(Block.TweetId))
+            {
+                var arg = new {tweetId = Block.TweetId, id = Block.Id, instance = DotNetObjectReference.Create(this)};
+                await JsRuntime.InvokeAsync<string>("Blockly.Twitter.render", arg);
+            }
+            else
+            {
+                var arg = new {id = Block.Id};
+                await JsRuntime.InvokeAsync<string>("Blockly.Twitter.clear", arg);
+            }
         }
     }
 }
