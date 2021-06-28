a49778ec9265090ff0fd1ed7aa673fdb1a807ea5
George Drak <sonicgd@gmail.com>
2021-06-28 13:58:49 +0500

    Use RegisterJsonEnumerableConversion from Sitko.Core

--- src/Sitko.Blockly.EntityFrameworkCore/BlockyEntityExtensions.cs
+++ src/Sitko.Blockly.EntityFrameworkCore/BlockyEntityExtensions.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
+﻿using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
-using Newtonsoft.Json;
+using Sitko.Core.Db.Postgres;
 
 namespace Sitko.Blockly.EntityFrameworkCore
 {
@@ -16,46 +12,9 @@ namespace Sitko.Blockly.EntityFrameworkCore
         {
             modelBuilder.RegisterJsonEnumerableConversion<TEntity, ContentBlock, List<ContentBlock>>(
                 entity => entity.Blocks,
-                fieldName);
+                fieldName, false);
 
             return modelBuilder;
-        }
-        
-        // TODO: DROP AFTER NEW SITKO.CORE RELEASE
-        public static void RegisterJsonEnumerableConversion<TEntity, TData, TEnumerable>(this ModelBuilder modelBuilder,
-            Expression<Func<TEntity, TEnumerable>> getProperty, string name)
-            where TEntity : class
-            where TEnumerable : IEnumerable<TData>, new()
-        {
-            var valueComparer = new ValueComparer<TEnumerable>(
-                (c1, c2) => c1.SequenceEqual(c2),
-                c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v!.GetHashCode())),
-                c => Deserialize<TEnumerable>(Serialize(c)!)!);
-            modelBuilder
-                .Entity<TEntity>()
-                .Property(getProperty)
-                .HasColumnType("jsonb")
-                .HasColumnName(name)
-                .HasConversion(data => Serialize(data),
-                    json => Deserialize<TEnumerable>(json) ?? new TEnumerable())
-                .Metadata.SetValueComparer(valueComparer);
-        }
-
-        private static JsonSerializerSettings _jsonSettings = new JsonSerializerSettings
-        {
-            ReferenceLoopHandling = ReferenceLoopHandling.Ignore,
-            TypeNameHandling = TypeNameHandling.Auto,
-            MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead
-        };
-
-        private static string Serialize(object obj)
-        {
-            return JsonConvert.SerializeObject(obj, _jsonSettings);
-        }
-
-        private static T? Deserialize<T>(string json)
-        {
-            return JsonConvert.DeserializeObject<T>(json, _jsonSettings);
         }
     }
 }
