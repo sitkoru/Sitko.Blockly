4039e2dbfc5d484acb0e3126c479d4de34d71818
George Drak <sonicgd@gmail.com>
2021-06-29 14:59:37 +0500

    Set current culture based on request

--- apps/Sitko.Blockly.Demo/Startup.cs
+++ apps/Sitko.Blockly.Demo/Startup.cs
@@ -1,5 +1,8 @@
-﻿using AntDesign.ProLayout;
+﻿using System.Collections.Generic;
+using System.Globalization;
+using AntDesign.ProLayout;
 using FluentValidation;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +24,14 @@ namespace Sitko.Blockly.Demo
             {
                 settings.Title = "Blockly";
             });
+        }
+
+        protected override void ConfigureAfterRoutingMiddleware(IApplicationBuilder app)
+        {
+            base.ConfigureAfterRoutingMiddleware(app);
+            app.UseRequestLocalization(new RequestLocalizationOptions()
+                .AddSupportedCultures("en-US", "ru")
+                .AddSupportedUICultures("en-US", "ru"));
         }
     }
 }
