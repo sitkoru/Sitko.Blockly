3be3bb0925bb1ada822e49e003a0687668c79dbd
George Drak <sonicgd@gmail.com>
2021-07-06 09:21:09 +0500

    Cleanup

--- src/Sitko.Blockly.Blazor/Forms/BlocklyForm.cs
+++ src/Sitko.Blockly.Blazor/Forms/BlocklyForm.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
 using Microsoft.JSInterop;
-using Sitko.Blockly.Blocks;
 using Sitko.Core.App.Blazor.Forms;
 using Sitko.Core.App.Collections;
 
