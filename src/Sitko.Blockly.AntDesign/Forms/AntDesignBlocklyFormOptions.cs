5e01cb275254e819c7e65462ecb4587875b71165
George Drak <sonicgd@gmail.com>
2021-07-22 10:24:20 +0500

    Upd CKEditor bundle, allow to configure CKEditor via form options

--- src/Sitko.Blockly.AntDesign/Forms/AntDesignBlocklyFormOptions.cs
+++ src/Sitko.Blockly.AntDesign/Forms/AntDesignBlocklyFormOptions.cs
@@ -2,7 +2,10 @@
 
 namespace Sitko.Blockly.AntDesignComponents.Forms
 {
+    using Sitko.Blazor.CKEditor;
+
     public class AntDesignBlocklyFormOptions : BlazorBlocklyFormOptions
     {
+        public CKEditorConfig? CKEditorConfig { get; set; }
     }
 }
