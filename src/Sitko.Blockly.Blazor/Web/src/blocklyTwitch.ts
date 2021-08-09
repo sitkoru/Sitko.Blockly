685be5a93f318dc026332db06f7eeb80950181d2
George Drak <sonicgd@gmail.com>
2021-08-09 11:33:01 +0500

    Don't need to load twitch js from web

--- src/Sitko.Blockly.Blazor/Web/src/blocklyTwitch.ts
+++ src/Sitko.Blockly.Blazor/Web/src/blocklyTwitch.ts
@@ -2,38 +2,11 @@ import {TwitchPlayer} from "twitch-player";
 import {BlocklyBase} from "./base";
 
 class BlocklyTwitch extends BlocklyBase {
-  private _isLoaded = false;
-  private _onReady: Function[] = [];
-
   constructor() {
     super('BlocklyTwitch');
   }
 
-  private load() {
-    let d = document;
-    let id = "twitch-js";
-    let js, fjs = d.getElementsByTagName('script')[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement('script');
-    js.id = id;
-    js.src = "https://player.twitch.tv/js/embed/v1.js";
-    fjs.parentNode.insertBefore(js, fjs);
-    js.addEventListener('load', () => {
-      this._isLoaded = true;
-      this._onReady.forEach(request => {
-        request();
-      });
-    });
-  }
-
   public render(container: HTMLElement, video?: string, channel?: string, collection?: string) {
-    if (!this._isLoaded) {
-      this.load();
-      this._onReady.push(() => {
-        this.render(container, video, channel, collection);
-      });
-      return;
-    }
     this.debug('render twitch', container, video, channel, collection);
     container.innerHTML = '';
     TwitchPlayer.FromOptions(container.id, {
