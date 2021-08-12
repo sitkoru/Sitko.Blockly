4751322a1538d9d09f24ac19426f49616e5462d2
George Drak <sonicgd@gmail.com>
2021-08-12 23:53:48 +0500

    Add extension for CompareNetObjects configuration

---
+++ src/Sitko.Blockly.Blazor/Extensions/CompareExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using KellermanSoftware.CompareNetObjects;
+using JetBrains.Annotations;
+
+namespace Sitko.Blockly.Blazor.Extensions
+{
+    [PublicAPI]
+    public static class CompareExtensions
+    {
+        public static ComparisonConfig AddBlocklyCollectionMapping(this ComparisonConfig config)
+        {
+            config.IgnoreCollectionOrder = true;
+            config.CollectionMatchingSpec ??= new Dictionary<Type, IEnumerable<string>>();
+            config.CollectionMatchingSpec.Add(typeof(ContentBlock), new[] { nameof(ContentBlock.Id) });
+            return config;
+        }
+    }
+}
