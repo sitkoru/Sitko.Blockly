ce46946214c656d07637b5299c0d771f0845938f
George Drak <sonicgd@gmail.com>
2021-09-08 16:12:53 +0500

    fix(demo): configure form comparison for blocks

--- apps/Sitko.Blockly.Demo/Pages/PostForm.cs
+++ apps/Sitko.Blockly.Demo/Pages/PostForm.cs
@@ -7,8 +7,10 @@ using Sitko.Core.App.Blazor.Forms;
 
 namespace Sitko.Blockly.Demo.Pages
 {
+    using Blazor.Extensions;
     using Core.Blazor.AntDesignComponents.Components;
     using Data.Repositories;
+    using KellermanSoftware.CompareNetObjects;
     using Microsoft.AspNetCore.Components;
 
     public class PostForm : BaseAntRepositoryForm<Post, Guid, PostsRepository>
@@ -16,6 +18,12 @@ namespace Sitko.Blockly.Demo.Pages
         [Parameter] public RenderFragment<PostForm> ChildContent { get; set; } = null!;
 
         protected override RenderFragment ChildContentFragment => ChildContent(this);
+
+        protected override void ConfigureComparer(ComparisonConfig comparisonConfig)
+        {
+            base.ConfigureComparer(comparisonConfig);
+            comparisonConfig.AddBlocklyCollectionMapping();
+        }
     }
 
     public class PostFormValidator : AbstractBlocklyFormValidator<Post>
