313d445a17bcfac21a1251628a99b34837bcf818
George Drak <sonicgd@gmail.com>
2022-01-17 18:30:08 +0500

    fix(mudblazor): rename extensions methods

--- src/Sitko.Blockly.MudBlazor/ApplicationExtensions.cs
+++ src/Sitko.Blockly.MudBlazor/ApplicationExtensions.cs
@@ -6,12 +6,12 @@ namespace Sitko.Blockly.MudBlazorComponents;
 [PublicAPI]
 public static class ApplicationExtensions
 {
-    public static Application AddAntDesignBlockly(this Application application,
+    public static Application AddMudBlazorBlockly(this Application application,
         Action<IApplicationContext, MudBlazorBlocklyModuleOptions> configure,
         string? configKey = null) =>
         application.AddModule<MudBlazorBlocklyModule, MudBlazorBlocklyModuleOptions>(configure, configKey);
 
-    public static Application AddAntDesignBlockly(this Application application,
+    public static Application AddMudBlazorBlockly(this Application application,
         Action<MudBlazorBlocklyModuleOptions>? configure = null, string? configKey = null) =>
         application.AddModule<MudBlazorBlocklyModule, MudBlazorBlocklyModuleOptions>(configure, configKey);
 }
