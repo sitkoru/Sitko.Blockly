819da3678e9e841b9885801e0e4191a13ee98f98
Alexander <zubkov_95@mail.ru>
2022-01-20 14:42:06 +0500

    fix: add custom twitch icon

--- src/Sitko.Blockly.MudBlazor/Blocks/MudTwitchBlockDescriptor.cs
+++ src/Sitko.Blockly.MudBlazor/Blocks/MudTwitchBlockDescriptor.cs
@@ -16,5 +16,5 @@ public record
     {
     }
 
-    public override string Icon => Icons.Filled.Stream; // TODO: Get twitch icon
+    public override string Icon => "<svg xmlns=\"http://www.w3.org/2000/svg\" xmlns:xlink=\"http://www.w3.org/1999/xlink\" aria-hidden=\"true\" role=\"img\" width=\"1em\" height=\"1em\" preserveAspectRatio=\"xMidYMid meet\" viewBox=\"0 0 24 24\"><path d=\"M11.64 5.93h1.43v4.28h-1.43m3.93-4.28H17v4.28h-1.43M7 2L3.43 5.57v12.86h4.28V22l3.58-3.57h2.85L20.57 12V2m-1.43 9.29l-2.85 2.85h-2.86l-2.5 2.5v-2.5H7.71V3.43h11.43z\" fill=\"currentColor\"/></svg>";
 }
