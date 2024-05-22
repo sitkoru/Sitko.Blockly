db153e9a848ccc4e3dc4307a41c7694c3f800a67
Степан <slepcenkostepan403@gmail.com>
2024-05-22 17:44:27 +0500

    feat: add blocks to extensions

--- apps/Sitko.Blockly.Demo.Client/EditorJsExtensions.cs
+++ apps/Sitko.Blockly.Demo.Client/EditorJsExtensions.cs
@@ -3,10 +3,13 @@ using Sitko.EditorJS;
 using Sitko.EditorJS.Blocks.Header;
 using Sitko.EditorJS.Blocks.Image;
 using Sitko.EditorJS.Blocks.List;
+using Sitko.EditorJS.Blocks.Marker;
 using Sitko.EditorJS.Blocks.Paragraph;
 using Sitko.EditorJS.Blocks.Quote;
 using Sitko.EditorJS.Blocks.SimpleImage;
+using Sitko.EditorJS.Blocks.Strikethrough;
 using Sitko.EditorJS.Blocks.Table;
+using Sitko.EditorJS.Blocks.Underline;
 
 namespace Sitko.Blockly.Demo.Client;
 
@@ -15,12 +18,16 @@ public static class EditorJsExtensions
     public static IServiceCollection AddEditorJSBlocks(this IServiceCollection serviceCollection)
     {
         serviceCollection.AddEditorJS()
+            .AddBlock<StrikethroughBlock, StrikethroughBlockOptions>()
+            .AddBlock<MarkerBlock, MarkerBlockOptions>()
+            .AddBlock<UnderlineTool, UnderlineBlockOptions>()
             .AddBlock<QuoteBlock, QuoteBlockOptions>()
             .AddBlock<TableBlock, TableBlockOptions>()
             .AddBlock<ListBlock, ListBlockOptions>()
             .AddBlock<ParagraphBlock, ParagraphBlockOptions>()
             .AddBlock<HeaderBlock, HeaderBlockOptions>()
             .AddBlock<SimpleImageBlock, SimpleImageBlockOptions>()
+            .AddGalleryBlock<StorageItem>()
             .AddImageBlock<StorageItem>();
         return serviceCollection;
     }
--- src/Sitko.EditorJS/Blocks/Image/BuilderExtensions.cs
+++ src/Sitko.EditorJS/Blocks/Image/BuilderExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Sitko.EditorJS.Blocks.Gallery;
 using Sitko.EditorJS.Configuration;
 
 namespace Sitko.EditorJS.Blocks.Image;
@@ -8,4 +9,8 @@ public static class BuilderExtensions
     public static IEditorJSBuilder AddImageBlock<TData>(this IEditorJSBuilder builder,
         Action<IConfiguration, ImageBlockOptions<TData>>? configure = null) where TData : class, new() =>
         builder.AddBlock<ImageBlock<TData>, ImageBlockOptions<TData>>(configure);
+
+    public static IEditorJSBuilder AddGalleryBlock<TData>(this IEditorJSBuilder builder,
+        Action<IConfiguration, GalleryBlockOptions<TData>>? configure = null) where TData : class, new() =>
+        builder.AddBlock<GalleryBlock<TData>, GalleryBlockOptions<TData>>(configure);
 }
