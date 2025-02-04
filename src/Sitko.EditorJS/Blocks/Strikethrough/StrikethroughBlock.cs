8476b1036b7e4e9c2c5d3ec7d238fe91324cc6a7
Степан <slepcenkostepan403@gmail.com>
2025-02-04 21:22:05 +0500

    fix: load gallery and strikethrough blocks from cdn

--- src/Sitko.EditorJS/Blocks/Strikethrough/StrikethroughBlock.cs
+++ src/Sitko.EditorJS/Blocks/Strikethrough/StrikethroughBlock.cs
@@ -5,6 +5,6 @@ public record StrikethroughBlock : ContentBlock;
 
 public record StrikethroughBlockOptions : ContentBlockOptions<StrikethroughBlock>
 {
-    public override string ScriptUrl { get; set; } = "/_content/Sitko.EditorJS.Blazor/strikethrough.js";
+    public override string ScriptUrl { get; set; } = "https://cdn.jsdelivr.net/npm/@sotaproject/strikethrough@latest";
     public override string ClassName { get; set; } = "Strikethrough";
 }
